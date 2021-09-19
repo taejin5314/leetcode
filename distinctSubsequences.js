7f304b6bff44c506415c21601932be6b21a87645
taejin <taejin5314@gmail.com>
2021-09-19 18:25:01 -0400

    update distinct subsequences

--- distinctSubsequences.js
+++ distinctSubsequences.js
@@ -4,12 +4,22 @@
  * @return {number}
  */
 var numDistinct = function (s, t) {
-  for (let i = 0; i < s.length - t.length; i++) {
-    let count = 0;
-    for (let j = 0; j < t.length; j++) {
+  let sLength = s.length;
+  let tLength = t.length;
+  const arr = Array(tLength).fill(0);
+
+  for (let i = sLength - 1; i >= 0; i--) {
+    let prev = 1;
+    for (let j = tLength - 1; j >= 0; j--) {
+      let cur = arr[j];
       if (s[i] === t[j]) {
-        count++;
+        arr[j] += prev;
       }
+      prev = cur;
     }
   }
+  return arr[0];
 };
+
+console.log(numDistinct('rabbbit', 'rabbit'));
+console.log(numDistinct('babgbag', 'bag'));
