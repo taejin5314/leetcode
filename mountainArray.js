841ea56bc102aad5c2f6d30558fa7365c733523f
taejin <taejin5314@gmail.com>
2021-09-20 17:54:56 -0400

    submit mountainArray and testing fibonacci

--- mountainArray.js
+++ mountainArray.js
@@ -1,3 +1,21 @@
 var validMountainArray = function (arr) {
+  let left = 0;
+  let right = arr.length - 1;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[left] < arr[left + 1]) {
+      left++;
+    }
+    if (arr[right - 1] > arr[right]) {
+      right--;
+    }
 
+    if (left === right && left !== 0 && right !== 0 && left !== arr.length - 1 && right !== arr.length - 1) {
+      return true;
+    }
+  }
+  return false;
 };
+
+console.log(validMountainArray([2, 1]));
+console.log(validMountainArray([3, 5, 5]));
+console.log(validMountainArray([0, 3, 2, 1]));
--- test.js
+++ test.js
@@ -1,20 +1,46 @@
+// var time = function (n) {
+//   var t0 = performance.now();
+//   var fib = function (n) {
+//     let cur, prev = 1, prevPrev = 0;
+//     if (n === 0 || n === 1) {
+//       return n;
+//     } else {
+//       for (let i = 2; i <= n; i++) {
+//         cur = prev + prevPrev;
+//         prevPrev = prev;
+//         prev = cur;
+//       }
+//       return cur;
+//     }
+//   };
+//   fib(n);
+//   console.log(performance.now() - t0);
+// }
+
 var time = function (n) {
   var t0 = performance.now();
-  var fib = function (n) {
-    let cur, prev = 1, prevPrev = 0;
-    if (n === 0 || n === 1) {
-      return n;
+  const map = { 1: 1, 2: 1 }
+  var fib = function (n, map) {
+    if (!map[n]) {
+      return map[n] = fib(n - 2, map) + fib(n - 1, map);
     } else {
-      for (let i = 2; i <= n; i++) {
-        cur = prev + prevPrev;
-        prevPrev = prev;
-        prev = cur;
-      }
-      return cur;
+      return map[n]
     }
   };
-  fib(n);
+  console.log(fib(n, map));
   console.log(performance.now() - t0);
 }
 
-time(35)
+const map = { 1: 1, 2: 1 }
+var fib = function (n, map) {
+  if (!map[n]) {
+    map[n] = fib(n - 2, map) + fib(n - 1, map);
+    return map[n]
+  } else {
+    return map[n]
+  }
+};
+
+console.log(fib(35, map))
+
+// time(35)
