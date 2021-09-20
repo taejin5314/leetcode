08c3f9008bb8e38c75c91c7934351859225d66a3
taejin <taejin5314@gmail.com>
2021-09-20 18:26:52 -0400

    fibonacci bottom-up

--- test.js
+++ test.js
@@ -31,16 +31,31 @@ var time = function (n) {
   console.log(performance.now() - t0);
 }
 
-const map = { 1: 1, 2: 1 }
-var fib = function (n, map) {
-  if (!map[n]) {
-    map[n] = fib(n - 2, map) + fib(n - 1, map);
-    return map[n]
-  } else {
-    return map[n]
-  }
-};
+// const map = { 1: 1, 2: 1 }
+// var fib = function (n, map) {
+//   if (!map[n]) {
+//     map[n] = fib(n - 2, map) + fib(n - 1, map);
+//     return map[n]
+//   } else {
+//     return map[n]
+//   }
+// };
 
-console.log(fib(35, map))
+var fib = function (n) {
+  let arr = new Array(n);
+  arr[0] = 1;
+  arr[1] = 1;
+
+  if (n === 0 || n === 1) {
+    return arr[n];
+  } else {
+    for (let i = 2; i <= n; i++) {
+      arr[i] = arr[i - 2] + arr[i - 1];
+    }
+    return arr[n];
+  }
+}
+
+console.log(fib(35))
 
 // time(35)
