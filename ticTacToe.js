63e13792986d28435d5932f9adf0872a6119693c
taejin <taejin5314@gmail.com>
2021-09-20 13:10:22 -0400

    daily challenge - tic tac toe

---
+++ ticTacToe.js
@@ -0,0 +1,8 @@
+var tictactoe = function (moves) {
+
+};
+
+console.log(tictactoe([[0, 0], [2, 0], [1, 1], [2, 1], [2, 2]]));
+console.log(tictactoe([[0, 0], [1, 1], [0, 1], [0, 2], [1, 0], [2, 0]]));
+console.log(tictactoe([[0, 0], [1, 1], [2, 0], [1, 0], [1, 2], [2, 1], [0, 1], [0, 2], [2, 2]]));
+console.log(tictactoe([[0, 0], [1, 1]]));
