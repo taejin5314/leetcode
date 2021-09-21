403cd62a7669efce021a833ba3ede736629840e3
taejin <taejin5314@gmail.com>
2021-09-21 18:23:25 -0400

    submit height checker

--- heightChecker.py
+++ heightChecker.py
@@ -2,7 +2,14 @@ from typing import List
 
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
-      return 0
+      heightsCopy = heights.copy();
+      heights.sort()
+      count = 0
+
+      for i in range(len(heights)):
+        if heightsCopy[i] != heights[i]:
+          count += 1
+      return count
 
 problem = Solution()
 print(problem.heightChecker([1, 1, 4, 2, 1, 3]))
