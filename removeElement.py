c587ab46aca7c6af102c07241bbc06b284a533ba
taejin <taejin5314@gmail.com>
2021-09-21 14:48:19 -0400

    leetcode daily challenge

--- removeElement.py
+++ removeElement.py
@@ -2,7 +2,11 @@ from typing import List
 
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-      return 0
+      arrCopy = nums.copy()
+      for i in arrCopy:
+        if i == val:
+          nums.remove(i)
+      return nums
 
 problem = Solution()
-print(problem.removeElement([3,2,2,3]))
+print(problem.removeElement([3,2,2,3], 2))
