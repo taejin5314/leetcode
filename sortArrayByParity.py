6268cc365f81132d713829250616a61272450616
taejin <taejin5314@gmail.com>
2021-09-21 14:22:26 -0400

    solved

--- sortArrayByParity.py
+++ sortArrayByParity.py
@@ -2,11 +2,15 @@ from typing import List
 
 class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
-        for i in nums:
-            if i % 2 == 0:
-                nums.remove(i)
-                nums.insert(0, i)
-        return nums
+      start, end = 0, len(nums) - 1
+
+      while start <= end:
+        if (nums[start] % 2 == 0):
+          start += 1
+        else:
+          nums[start], nums[end] = nums[end], nums[start]
+          end -= 1
+      return nums
 
 problem = Solution()
 print(problem.sortArrayByParity([3, 2, 4, 1]))
