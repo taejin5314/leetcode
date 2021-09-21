694254966e7b88dba8d4d7e037ebf1a6eed5086f
taejin <taejin5314@gmail.com>
2021-09-21 18:16:12 -0400

    initial code for height checker leetcode

---
+++ heightChecker.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+      return 0
+
+problem = Solution()
+print(problem.heightChecker([1, 1, 4, 2, 1, 3]))
+print(problem.heightChecker([5, 1, 2, 3, 4]))
+print(problem.heightChecker([1, 2, 3, 4, 5]))
