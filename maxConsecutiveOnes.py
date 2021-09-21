d1b372d56c841c9066034fa9957f9715432ba446
taejin <taejin5314@gmail.com>
2021-09-21 16:14:33 -0400

    leetcode daily challenge

--- maxConsecutiveOnes.py
+++ maxConsecutiveOnes.py
@@ -2,14 +2,14 @@ from typing import List
 
 class Solution:
   def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
-    count, maximum = 1, 0
-    for i in range(1, len(nums)):
-      if nums[i] != nums[i-1]:
-        count = 1
+    count, maximum = 0, 0
+    for i in range(len(nums)):
+      if nums[i] != 1:
+        count = 0
       else:
         count += 1
       maximum = max(maximum, count)
     return maximum
 
 problem = Solution()
-print(problem.findMaxConsecutiveOnes([1, 1, 0, 1, 1, 1]))
+print(problem.findMaxConsecutiveOnes([1, 0, 1, 1, 0, 1]))
