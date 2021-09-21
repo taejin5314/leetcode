3eff240a01ba70ca6028bd5aecf94c81c6228b67
taejin <taejin5314@gmail.com>
2021-09-21 18:39:59 -0400

    submit third maximum number

--- thirdMaximumNumber.py
+++ thirdMaximumNumber.py
@@ -1,3 +1,16 @@
+from typing import List
+
 class Solution:
-    def thirdMax(self, nums: List[int]) -> int:
-        
+  def thirdMax(self, nums: List[int]) -> int:
+    arr = []
+    for i,c in enumerate(nums):
+      if len(arr) < 3 and c not in arr:
+        arr.append(c)
+      else:
+        if c not in arr and min(arr) < c:
+          arr[arr.index(min(arr))] = c
+
+    return min(arr) if len(arr) >= 3 else max(arr)
+
+problem = Solution()
+print(problem.thirdMax([2, 2, 3, 1]))
