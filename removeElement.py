b04f37b685a078dcef213fbc838cc91fe5647e06
taejin <taejin5314@gmail.com>
2021-09-21 14:24:14 -0400

    start remove element

---
+++ removeElement.py
@@ -0,0 +1,8 @@
+from typing import List
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+      return 0
+
+problem = Solution()
+print(problem.removeElement([3,2,2,3]))
