38a7c5f9d4563d916d2f9088872e5d3ef11dea6f
taejin <taejin5314@gmail.com>
2021-09-22 19:14:00 -0400

    initial code for find all numbers disappeared in an array

---
+++ findAllNumbersDisappear.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+  def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+    return 0
+
+problem = Solution()
+print(problem.findDisappearedNumbers([4,3,2,7,8,2,3,1]))
+print(problem.findDisappearedNumbers([1,1]))
