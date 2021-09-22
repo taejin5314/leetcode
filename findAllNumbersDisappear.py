98d74186c36318b0902998b3e88f8d437d04746f
taejin <taejin5314@gmail.com>
2021-09-22 19:51:09 -0400

    submit find all numbers disappeared in an array

--- findAllNumbersDisappear.py
+++ findAllNumbersDisappear.py
@@ -2,7 +2,12 @@ from typing import List
 
 class Solution:
   def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-    return 0
+    map = {}
+    for i, c in enumerate(nums):
+      map[c] = 1
+      if i+1 not in map.keys():
+        map[i+1] = 0
+    return [arr for arr in map.keys() if map[arr] == 0]
 
 problem = Solution()
 print(problem.findDisappearedNumbers([4,3,2,7,8,2,3,1]))
