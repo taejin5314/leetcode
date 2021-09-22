25d334d5764a7ed3b56a5d4a8a41fdf00baaad70
taejin <taejin5314@gmail.com>
2021-09-22 15:06:44 -0400

    submit maximum length of concatenated string

--- maximumLengthOfConcatenated.py
+++ maximumLengthOfConcatenated.py
@@ -2,7 +2,20 @@ from typing import List
 
 class Solution:
   def maxLength(self, arr: List[str]) -> int:
-    return 0
+    maximum = 0
+    combinations = ['']
+    for i in range(len(arr)):
+      for j in range(len(combinations)):
+        if len(set(arr[i] + combinations[j])) == len(arr[i] + combinations[j]):
+          combinations.append(arr[i] + combinations[j])
+          maximum = max(len(arr[i] + combinations[j]), maximum)
+    return maximum
 
 problem = Solution()
 print(problem.maxLength(["un","iq","ue"]))
+print(problem.maxLength(["cha","r","act","ers"]))
+print(problem.maxLength(["abcdefghijklmnopqrstuvwxyz"]))
+print(problem.maxLength(["a", "abc", "d", "de", "def"]))
+print(problem.maxLength(["yy","bkhwmpbiisbldzknpm"]))
+print(problem.maxLength(["ogud","ejipchfydrgl","b","kjxmzhnuoisgqvawel","mizlcgdqebwuotfnk","bjvkrgeozidytquchlw","tzjqwumkirxeal","x","rkpuabmo","mxndpcqzua"]))
+print(problem.maxLength(["ab","cd","cde","cdef","efg","fgh","abxyz"]))
