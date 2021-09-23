f1c0f51c9f55748152df2c4399bbeef439fbd3f7
taejin <taejin5314@gmail.com>
2021-09-22 21:29:50 -0400

    initial code for swap nodes in pairs leetcode

---
+++ swapNodesPairs.py
@@ -0,0 +1,13 @@
+from typing import Optional
+
+class ListNode:
+  def __init__(self, val=0, next=None):
+    self.val = val
+    self.next = next
+
+class Solution:
+  def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    return 0
+
+problem = Solution()
+print(problem.swapPairs([1, 2, 3, 4]))
