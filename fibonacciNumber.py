bcbed88d7f08aadc5a06777b7c80957a5d099dcf
taejin <taejin5314@gmail.com>
2021-09-23 16:35:18 -0400

    submit memoization fib

--- fibonacciNumber.py
+++ fibonacciNumber.py
@@ -1,11 +1,19 @@
 class Solution:
   def fib(self, n: int) -> int:
-    arr = [1 for i in range(n)]
+    arr = [1 for i in range(n + 1)]
+    arr[0] = 0
         
-    for i in range(2, n):
+    for i in range(2, n + 1):
       arr[i] = arr[i - 2] + arr[i - 1]
         
-    return arr[n-1]
+    return arr[n]
 
 problem = Solution()
+print(problem.fib(0))
+print(problem.fib(1))
+print(problem.fib(2))
 print(problem.fib(3))
+print(problem.fib(4))
+print(problem.fib(5))
+print(problem.fib(6))
+print(problem.fib(7))
