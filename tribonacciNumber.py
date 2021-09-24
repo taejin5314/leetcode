b332d1cf2e0a740b8b15f0610c13a3f59b30067d
taejin <taejin5314@gmail.com>
2021-09-24 16:06:19 -0400

    submit tribonacci

--- tribonacciNumber.py
+++ tribonacciNumber.py
@@ -1,6 +1,20 @@
 class Solution:
   def tribonacci(self, n: int) -> int:
-    return 0
+    if n == 0:
+      return 0
+    elif n == 1:
+      return 1
+    elif n == 2:
+      return 1
+    else:
+      prevPrevPrev, prevPrev, prev = 0, 1, 1
+      for i in range(3, n+1):
+        cur = prev + prevPrev + prevPrevPrev
+        prevPrevPrev = prevPrev
+        prevPrev = prev
+        prev = cur
+      return cur
 
 problem = Solution()
 print(problem.tribonacci(4))
+print(problem.tribonacci(25))
