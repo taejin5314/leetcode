eb9b2c405f6fbfbd3fa3873233b3a54890a3fcea
taejin <taejin5314@gmail.com>
2021-09-25 17:00:03 -0400

    leetcode study plan

---
+++ twoSumII.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+  def twoSum(self, numbers: List[int], target: int) -> List[int]:
+    return 0
+
+problem = Solution()
+print(problem.twoSum([2, 7, 11, 15], 9))
+print(problem.twoSum([2, 3, 4], 6))
+print(problem.twoSum([-1, 0], -1))
