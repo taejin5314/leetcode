79865f184035ffededa2f17c35670ce4d4189e6c
taejin <taejin5314@gmail.com>
2021-09-25 17:03:11 -0400

    submit daily study plan leetcode

--- twoSumII.py
+++ twoSumII.py
@@ -2,7 +2,14 @@ from typing import List
 
 class Solution:
   def twoSum(self, numbers: List[int], target: int) -> List[int]:
-    return 0
+    left, right = 0, len(numbers) - 1
+    while left <= right:
+      if numbers[left] + numbers[right] > target:
+        right -= 1
+      elif numbers[left] + numbers[right] < target:
+        left += 1
+      else:
+        return [left + 1, right + 1]
 
 problem = Solution()
 print(problem.twoSum([2, 7, 11, 15], 9))
