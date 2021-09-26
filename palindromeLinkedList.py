8f9554949a8d398313443f9ee7822ab25e41d3af
taejin <taejin5314@gmail.com>
2021-09-26 15:35:27 -0400

    submit palindrome linked list

--- palindromeLinkedList.py
+++ palindromeLinkedList.py
@@ -1,3 +1,5 @@
+from typing import List, Optional
+
 # Definition for singly-linked list.
 class ListNode:
   def __init__(self, val=0, next=None):
@@ -6,4 +8,25 @@ class ListNode:
 
 class Solution:
   def isPalindrome(self, head: Optional[ListNode]) -> bool:
-    return 0
+    slow = fast = head
+    while fast and fast.next:
+      slow = slow.next
+      fast = fast.next.next
+    
+    right = None
+    while slow:
+      nxt = slow.next
+      slow.next = right
+      right = slow
+      slow = nxt
+    
+    while right:
+      if right.val != head.val:
+        return False
+      right = right.next
+      head = head.next
+    return True
+
+head = ListNode([1, 2, 2, 1])
+problem = Solution()
+print(problem.isPalindrome(head))
