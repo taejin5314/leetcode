bc7edcaa11096721ab1697ebfe91d229d03c3415
taejin <taejin5314@gmail.com>
2021-09-28 14:59:15 -0400

    submit daily challenge

--- sortArrayByParityII.py
+++ sortArrayByParityII.py
@@ -2,15 +2,17 @@ from typing import List
 
 class Solution:
   def sortArrayByParityII(self, nums: List[int]) -> List[int]:
-    start, end = 0, len(nums) - 1
+    result = [None for _ in range(len(nums))]
+    even, odd = 0, 1
 
-    while start <= end:
-      if (nums[start] % 2 == 0):
-        start += 1
-      else:
-        nums[start], nums[end] = nums[end], nums[start]
-        end -= 1
-    return nums
+    for i in range(len(nums)):
+      if (nums[i] % 2 == 0):
+        result[even] = nums[i]
+        even += 2
+      if (nums[i] % 2 == 1):
+        result[odd] = nums[i]
+        odd += 2
+    return result
 
 problem = Solution()
 print(problem.sortArrayByParityII([4, 2, 5, 7]))
