183847699d377a963754dbccc9ba6a350912b6c1
taejin <taejin5314@gmail.com>
2021-09-29 18:08:54 -0400

    submit flood fill

--- floodFill.py
+++ floodFill.py
@@ -2,9 +2,22 @@ from typing import List
 
 class Solution:
   def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
-    
-    image[sr][sc] = newColor
-    return 0
+    rows = len(image)
+    cols = len(image[0])
+    originalColor = image[sr][sc]
+
+    def dfs(r, c):
+      if r < 0 or c < 0 or r > rows - 1 or c > cols - 1 or image[r][c] == newColor or image[r][c] != originalColor:
+        return
+      image[r][c] = newColor
+
+      dfs(r + 1, c)
+      dfs(r - 1, c)
+      dfs(r, c + 1) 
+      dfs(r, c - 1)
+    dfs(sr, sc)
+
+    return image
 
 problem = Solution()
 print(problem.floodFill([[1, 1, 1], [1, 1, 0], [1, 0, 1]], 1, 1, 2))
