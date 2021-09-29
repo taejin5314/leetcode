83651a0fc303d53c5a2280b9a5f4f40d2fc007f3
taejin <taejin5314@gmail.com>
2021-09-29 16:25:41 -0400

    initial code for permutation in string

---
+++ permutationInString.py
@@ -0,0 +1,7 @@
+class Solution:
+  def checkInclusion(self, s1: str, s2: str) -> bool:
+    return 0
+
+problem = Solution()
+print(problem.checkInclusion('ab', 'eidbaooo'))
+print(problem.checkInclusion('ab', 'eidboaoo'))
