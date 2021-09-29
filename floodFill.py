b16007b9a70d8ea651052a140803db15b7175cc7
taejin <taejin5314@gmail.com>
2021-09-29 18:01:35 -0400

    update flood fill

--- floodFill.py
+++ floodFill.py
@@ -1,3 +1,11 @@
+from typing import List
+
 class Solution:
-    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
-        
+  def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
+    
+    image[sr][sc] = newColor
+    return 0
+
+problem = Solution()
+print(problem.floodFill([[1, 1, 1], [1, 1, 0], [1, 0, 1]], 1, 1, 2))
+print(problem.floodFill([[0, 0, 0], [0, 0, 0]], 0, 0, 2))
