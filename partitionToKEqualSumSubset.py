fec6db8e49d5569f989e29ca05ca18fb38efee2a
taejin <taejin5314@gmail.com>
2021-09-30 15:00:26 -0400

    daily leetcode challenge - Partition to K equal sum subset

---
+++ partitionToKEqualSumSubset.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+  def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
+    return 0
+
+problem = Solution()
+print(problem.canPartitionKSubsets([4,3,2,3,5,2,1], 4))
+print(problem.canPartitionKSubsets([1,2,3,4], 3))
