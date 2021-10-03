2d48439c23e3b0c49adb20cbc95e22bbc6ff7206
taejin <taejin5314@gmail.com>
2021-10-02 20:34:15 -0400

    submit daily challenge - jump game

--- jumpGame.py
+++ jumpGame.py
@@ -5,7 +5,7 @@ class Solution:
     lastGoodIndex = len(nums) - 1
 
     for i in range(len(nums) - 1, -1, -1):
-      if nums[i] + lastGoodIndex >= lastGoodIndex:
+      if nums[i] + i >= lastGoodIndex:
         lastGoodIndex = i
 
     return lastGoodIndex == 0
