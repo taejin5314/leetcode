4603158669f6f21429f8b8e2d5c10399c59ca554
taejin <taejin5314@gmail.com>
2021-10-02 20:30:39 -0400

    leetcode daily challenge - Jump game

---
+++ jumpGame.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+  def canJump(self, nums: List[int]) -> bool:
+    lastGoodIndex = len(nums) - 1
+
+    for i in range(len(nums) - 1, -1, -1):
+      if nums[i] + lastGoodIndex >= lastGoodIndex:
+        lastGoodIndex = i
+
+    return lastGoodIndex == 0
+
+problem = Solution()
+print(problem.canJump([2, 3, 1, 1, 4]))
