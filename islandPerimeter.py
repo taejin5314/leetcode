766181ebe095e08b21c4a199de5bfff9b43eed08
taejin <taejin5314@gmail.com>
2021-10-04 16:15:00 -0400

    submit island perimeter

--- islandPerimeter.py
+++ islandPerimeter.py
@@ -6,38 +6,15 @@ class Solution:
     cols = len(grid[0])
     perimeter = 0
 
-    def bfs(grid, i, j):
-      nonlocal perimeter
-      if i < 0 or i >= rows or j < 0 or j >= cols or grid[i][j] == 0:
-        return
-
-      grid[i][j] = 0
-
-      perimeter += 3
-
-      if i < rows - 1:
-        if grid[i+1][j] == 1:
-          perimeter -= 1
-      if i > 0:
-        if grid[i-1][j] == 1:
-          perimeter -= 1
-
-      if j < cols - 1:
-        if grid[i][j+1] == 1:
-          perimeter -= 1
-      if j > 0:
-        if grid[i][j-1] == 1:
-          perimeter -= 1
-
-      bfs(grid, i+1, j)
-      bfs(grid, i-1, j)
-      bfs(grid, i, j+1)
-      bfs(grid, i, j-1)
-
     for i in range(rows):
       for j in range(cols):
         if grid[i][j] == 1:
-          bfs(grid, i, j)
+          perimeter += 4
+
+          if i and grid[i-1][j]:
+            perimeter -= 2
+          if j and grid[i][j-1]:
+            perimeter -= 2
     
     return perimeter
 
