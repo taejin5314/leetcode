e3e39bff313a17b4737daaadd18194da9f43637f
taejin <taejin5314@gmail.com>
2021-10-05 20:21:46 -0400

    submit daily leetcode challenge

--- validParentheses.py
+++ validParentheses.py
@@ -1,6 +1,21 @@
 class Solution:
   def isValid(self, s: str) -> bool:
-    return 0
+    dict = { "{" : "}", "[" : "]", "(" : ")"}
+    stack = []
+    
+    for elm in s:
+      if elm in dict.keys():
+        stack.append(elm)
+      else:
+        if not stack:
+          return False
+        c = stack.pop()
+        if dict[c] != elm:
+          return False
+        
+    if stack:
+      return False
+    return True
 
 problem = Solution()
 print(problem.isValid("()"))
