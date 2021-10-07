fe870abde00d326f2730bbe818a172e4213de630
taejin <taejin5314@gmail.com>
2021-10-07 16:28:25 -0400

    submit word search

--- wordSearch.py
+++ wordSearch.py
@@ -6,34 +6,31 @@ class Solution:
     rows = len(board)
     cols = len(board[0])
 
-    def dfs(board, i, j, target):
-      if i < 0 or i >= rows or j < 0 or j >= cols or target[0] != board[i][j]:
-        return False
-
-      boardCopy = copy.deepcopy(board)
-
-      if boardCopy[i][j] == target[0]:
-        boardCopy[i][j] = "#"
-        target = target[1:]
-      else:
-        return
-
-      if len(target) == 0:
-        return True
-        
-      return dfs(boardCopy, i+1, j, target) or dfs(boardCopy, i-1, j, target) or dfs(boardCopy, i, j+1, target) or dfs(boardCopy, i, j-1, target)
-
     for i in range(rows):
       for j in range(cols):
         if board[i][j] == word[0]:
-          res = dfs(board, i, j, word)
-
-          if res:
+          if self.dfs(board, i, j, word):
             return True
     
     return False
+  
+  def dfs(self, board, i, j, word):
+    if len(word) == 0:
+      return True
+    if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or word[0] != board[i][j]:
+      return False
+
+    tmp = board[i][j]
+    board[i][j] = '#'
+      
+    res = self.dfs(board, i+1, j, word[1:]) or self.dfs(board, i-1, j, word[1:]) or self.dfs(board, i, j+1, word[1:]) or self.dfs(board, i, j-1, word[1:])
+    board[i][j] = tmp
+
+    return res
 
 problem = Solution()
 print(problem.exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], "ABCCED"))
 print(problem.exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], "SEE"))
 print(problem.exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], "ABCB"))
+
+print(problem.exist([["A","A","A","A","A","A"],["A","A","A","A","A","A"],["A","A","A","A","A","A"],["A","A","A","A","A","A"],["A","A","A","A","A","A"],["A","A","A","A","A","A"]], "AAAAAAAAAAAAAAB"))
