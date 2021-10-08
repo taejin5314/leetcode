96c11d74d160b95ab951ffdfacc0feb78be5ff5c
taejin <taejin5314@gmail.com>
2021-10-08 17:27:00 -0400

    prefix tree

--- implementTrie.py
+++ implementTrie.py
@@ -1,43 +1,102 @@
+# class Trie:
+#   def __init__(self):
+#     self.trie = {}
+
+#   def insert(self, word: str) -> None:
+#     tmp = self.trie
+#     for elm in word:
+#       if elm not in tmp:
+#         tmp[elm] = {}
+#       tmp = tmp[elm]
+
+#     tmp["end"] = True
+
+#   def search(self, word: str) -> bool:
+#     tmp = self.trie
+
+#     for elm in word:
+#       if elm in tmp:
+#         tmp = tmp[elm]
+#       else:
+#         return False
+
+#     return True if "end" in tmp else False
+
+#   def startsWith(self, prefix: str) -> bool:
+#     tmp = self.trie
+
+#     for elm in prefix:
+#       if elm in tmp:
+#         tmp = tmp[elm]
+#       else:
+#         return False
+    
+#     return True
+class Node:
+  def __init__(self):
+    self.next = {}
+    self.end = False
+    self.count = 0
+    
 class Trie:
   def __init__(self):
-    self.trie = {}
+    self.trieNode = Node()
 
   def insert(self, word: str) -> None:
-    tmp = self.trie
-    for elm in word:
-      if elm not in tmp:
-        tmp[elm] = {}
-      tmp = tmp[elm]
+    node = self.trieNode
 
-    tmp["end"] = True
+    for elm in word:
+      if elm not in node.next:
+        node.next[elm] = Node()
+      node.next[elm].count += 1
+      node = node.next[elm]
+    
+    node.end = True
 
   def search(self, word: str) -> bool:
-    tmp = self.trie
+    node = self.trieNode
 
     for elm in word:
-      if elm in tmp:
-        tmp = tmp[elm]
+      if elm in node.next:
+        node = node.next[elm]
       else:
         return False
-
-    return True if "end" in tmp else False
-
-  def startsWith(self, prefix: str) -> bool:
-    tmp = self.trie
+    
+    return node.end
+  
+  def startsWith(self, prefix: str) -> int:
+    node = self.trieNode
 
     for elm in prefix:
-      if elm in tmp:
-        tmp = tmp[elm]
+      if elm in node.next:
+        node = node.next[elm]
+      else:
+        return 0
+
+    return node.count
+  
+  def searchPattern(self, target: str) -> int:
+    node = self.trieNode
+
+    for i, elm in enumerate(target):
+      if elm == '.':
+        for c in node.next.keys():
+          print(c)
+      elif elm in node.next:
+        node = node.next[elm]
       else:
         return False
     
-    return True
-    
+    return node.count
+
+
 obj = Trie()
 obj.insert("apple")
 print(obj.search("app"))
-print(obj.search("apple"))
-print(obj.startsWith("app"))
+obj.insert("ape")
 obj.insert("app")
-print(obj.search("app"))
-print(obj.trie)
+print(obj.search("pp"))
+print(obj.search("apple"))
+print(obj.search("ap"))
+print(obj.startsWith("al"))
+print(obj.searchPattern("ap."))
