d6706c6fd6a588ac9f160ff399be51caed62e3a3
taejin <taejin5314@gmail.com>
2021-10-10 15:16:18 -0400

    daily challenge - bitwise and of numberse range

---
+++ bitwiseAndNumberRange.py
@@ -0,0 +1,8 @@
+class Solution:
+  def rangeBitwiseAnd(self, left: int, right: int) -> int:
+    return 0
+
+problem = Solution()
+print(problem.rangeBitwiseAnd(5, 7))
+print(problem.rangeBitwiseAnd(0, 0))
+print(problem.rangeBitwiseAnd(1, 2147483647))
