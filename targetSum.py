2bd20f7f09299e0cf337e23505cadf616e3f86da
taejin <taejin5314@gmail.com>
2021-10-10 16:06:35 -0400

    initial code for target sum

---
+++ targetSum.py
@@ -0,0 +1,8 @@
+from typing import List
+
+class Solution:
+  def findTargetSumWays(self, nums: List[int], target: int) -> int:
+    return nums
+
+problem = Solution()
+print(problem.findTargetSumWays([1,1,1,1,1], 3))
