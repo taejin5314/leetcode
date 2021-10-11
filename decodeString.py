059db59e0ca7da608cba4461cadc7024ee68c9b9
taejin <taejin5314@gmail.com>
2021-10-11 17:57:07 -0400

    submit decode string

--- decodeString.py
+++ decodeString.py
@@ -1,7 +1,21 @@
 class Solution:
   def decodeString(self, s: str) -> str:
-    for elm in s:
-      print(elm)
+    stack = []
+    for i, elm in enumerate(s):
+      if elm != ']':
+        stack.append(elm)
+      else:
+        substring = ""
+        while stack[-1] != "[":
+          substring = stack.pop() + substring
+        stack.pop()
+
+        repeat = ""
+        while stack and stack[-1].isdigit():
+          repeat = stack.pop() + repeat
+        stack.append(int(repeat) * substring)
+
+    return "".join(stack)
 
 problem = Solution()
 print(problem.decodeString("3[a]2[bc]"))
