8e45acd0a46b116f502bafd221246ef36412d119
taejin <taejin5314@gmail.com>
2021-10-12 13:34:02 -0400

    initial code for 01 matrix

---
+++ 01Matrix.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+  def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+    return 0
+
+problem = Solution()
+print(problem.updateMatrix([[0,0,0],[0,1,0],[0,0,0]]))
+print(problem.updateMatrix([[0,0,0],[0,1,0],[1,1,1]]))
