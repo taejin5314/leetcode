5351ba2a8964cbcc44c0e825c825b2f1f1801180
taejin <taejin5314@gmail.com>
2021-10-14 19:41:16 -0400

    01 matrix

--- 01Matrix.py
+++ 01Matrix.py
@@ -2,7 +2,7 @@ from typing import List
 
 class Solution:
   def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-    return 0
+    print(mat)
 
 problem = Solution()
 print(problem.updateMatrix([[0,0,0],[0,1,0],[0,0,0]]))
