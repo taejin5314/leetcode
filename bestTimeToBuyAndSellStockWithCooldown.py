816ca00acd0d3917b37310077524ed588ea111af
taejin <taejin5314@gmail.com>
2021-10-15 17:03:00 -0400

    solution of daily leetcode challenge - best time to buy and sell stock with cooldown

--- bestTimeToBuyAndSellStockWithCooldown.py
+++ bestTimeToBuyAndSellStockWithCooldown.py
@@ -2,7 +2,25 @@ from typing import List
 
 class Solution:
   def maxProfit(self, prices: List[int]) -> int:
-    return 0
+    result = {}
+  
+    def dfs(index, buying):
+      if index >= len(prices):
+        return 0
+      if (index, buying) in result:
+        return result[(index, buying)]
+      
+      cooldown = dfs(index + 1, buying)
+      if buying:
+        buy = dfs(index + 1, not buying) - prices[index]
+        result[(index, buying)] = max(buy, cooldown)
+      else:
+        sell = dfs(index + 2, not buying) + prices[index]
+        result[(index, buying)] = max(sell, cooldown)
+      return result[(index, buying)]
+  
+    return dfs(0, True)
+
 
 problem = Solution()
 print(problem.maxProfit([1, 2, 3, 0, 2]))
