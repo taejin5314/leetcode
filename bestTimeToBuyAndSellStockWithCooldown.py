79a93f738f1114cb27488c233c53193481e2209a
taejin <taejin5314@gmail.com>
2021-10-15 15:17:30 -0400

    leetcode daily challenge

---
+++ bestTimeToBuyAndSellStockWithCooldown.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+  def maxProfit(self, prices: List[int]) -> int:
+    return 0
+
+problem = Solution()
+print(problem.maxProfit([1, 2, 3, 0, 2]))
+print(problem.maxProfit([1]))
