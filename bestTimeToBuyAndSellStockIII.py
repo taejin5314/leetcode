e758079ea342b6bbcc017479880ee79b2402a9dc
taejin <taejin5314@gmail.com>
2021-10-16 15:22:54 -0400

    initial code for leetcode daily challenge

---
+++ bestTimeToBuyAndSellStockIII.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+  def maxProfit(self, prices: List[int]) -> int:
+    return 0
+
+problem = Solution()
+print(problem.maxProfit([3,3,5,0,0,3,1,4]))
+print(problem.maxProfit([1,2,3,4,5]))
+print(problem.maxProfit([7,6,4,3,1]))
+print(problem.maxProfit([1]))
