a61d6b9b10c441e3b79cbbb0dee6417e1f99e10e
taejin <taejin5314@gmail.com>
2021-10-16 20:42:38 -0400

    leetcode keys and rooms

---
+++ keysAndRooms.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+  def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+    return 0
+
+problem = Solution()
+print(problem.canVisitAllRooms([[1],[2],[3],[]]))
+print(problem.canVisitAllRooms([[1,3],[3,0,1],[2],[0]]))
