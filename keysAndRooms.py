2ab649989312c1df3e66679539925f3a802155cc
taejin <taejin5314@gmail.com>
2021-10-17 16:19:05 -0400

    submit keys and rooms

--- keysAndRooms.py
+++ keysAndRooms.py
@@ -1,9 +1,21 @@
 from typing import List
+from collections import defaultdict
 
 class Solution:
   def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+    visited = defaultdict(int, {0 : 1})
+    stack = [0]
+    count = 1
+
+    while stack:
+      keys = rooms[stack.pop()]
+      for k in keys:
+        if not visited[k]:
+          stack.append(k)
+          visited[k] = 1
+          count += 1
     
-    return 0
+    return count == len(rooms)
 
 problem = Solution()
 print(problem.canVisitAllRooms([[1],[2],[3],[]]))
