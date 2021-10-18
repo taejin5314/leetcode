c3add6881df49e5b83c8a1a7e0d4dd4f6639aeaf
taejin <taejin5314@gmail.com>
2021-10-18 14:31:14 -0400

    initial code for intersection of two arrays

---
+++ intersectionOfTwoArrays.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+  def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+    print(nums2)
+
+problem = Solution()
+print(problem.intersection([1, 2, 2, 1], [2, 2]))
+print(problem.intersection([4, 9 ,5], [9, 4, 9, 8, 4]))
