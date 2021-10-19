32da29fe29adbae281bba8083b76ba96c7af8694
taejin <taejin5314@gmail.com>
2021-10-19 13:33:47 -0400

    initial code for next greate element I

---
+++ nextGreateElementI.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+  def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+    print(nums1)
+
+problem = Solution()
+print(problem.nextGreaterElement([4,1,2], [1,3,4,2]))
+print(problem.nextGreaterElement([2,4], [1,2,3,4]))
