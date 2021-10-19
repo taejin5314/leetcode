daf6382342b4efacda0315a946b55ecaf9858064
taejin <taejin5314@gmail.com>
2021-10-19 13:57:57 -0400

    solution for next greate element I

--- nextGreateElementI.py
+++ nextGreateElementI.py
@@ -2,7 +2,22 @@ from typing import List
 
 class Solution:
   def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-    print(nums1)
+    hashMap = dict()
+    stack = []
+    result = []
+
+    for num in nums2:
+      if stack and stack[-1] < num:
+        hashMap[stack.pop()] = num
+      stack.append(num)
+
+    for num in nums1:
+      if num in hashMap:
+        result.append(hashMap[num])
+      else:
+        result.append(-1)
+
+    return result
 
 problem = Solution()
 print(problem.nextGreaterElement([4,1,2], [1,3,4,2]))
