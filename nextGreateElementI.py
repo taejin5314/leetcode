30cc8bc8735906ba5ebb4b68a6aedd807fc20dee
taejin <taejin5314@gmail.com>
2021-10-19 14:45:07 -0400

    submit leetcode daily

--- nextGreateElementI.py
+++ nextGreateElementI.py
@@ -7,18 +7,20 @@ class Solution:
     result = []
 
     for num in nums2:
-      if stack and stack[-1] < num:
-        hashMap[stack.pop()] = num
+      while stack and stack[-1] < num:
+        hashMap[stack[-1]] = num
+        stack.pop()
       stack.append(num)
 
+    for num in stack:
+      hashMap[num] = -1
+
     for num in nums1:
-      if num in hashMap:
-        result.append(hashMap[num])
-      else:
-        result.append(-1)
+      result.append(hashMap[num])
 
     return result
 
 problem = Solution()
 print(problem.nextGreaterElement([4,1,2], [1,3,4,2]))
 print(problem.nextGreaterElement([2,4], [1,2,3,4]))
+print(problem.nextGreaterElement([1, 3, 5, 2, 4], [6, 5, 4, 3, 2, 1, 7]))
