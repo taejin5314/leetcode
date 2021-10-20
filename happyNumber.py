083545d2b863b16d047f9bb3eadbcf5e806f4657
taejin <taejin5314@gmail.com>
2021-10-20 13:18:28 -0400

    submit happy number

--- happyNumber.py
+++ happyNumber.py
@@ -1,6 +1,20 @@
 class Solution:
   def isHappy(self, n: int) -> bool:
-    return n
+    def dfs(map, n):
+      if n == 1:
+        return True
+      if n in map:
+        return False
+
+      res = 0
+      map.append(n)
+
+      for num in str(n):
+        res += int(num) ** 2
+    
+      return dfs(map, res)
+    
+    return dfs([], n)
 
 problem = Solution()
 print(problem.isHappy(19))
