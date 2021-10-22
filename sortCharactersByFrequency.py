dd770c8a55ee9c20d00d9ddcd718a6f557c7279b
taejin <taejin5314@gmail.com>
2021-10-21 22:10:28 -0400

    leetcode daily challenge - sort characters by frequency

---
+++ sortCharactersByFrequency.py
@@ -0,0 +1,8 @@
+class Solution:
+  def frequencySort(self, s: str) -> str:
+    return 0
+
+problem = Solution()
+print(problem.frequencySort("tree"))
+print(problem.frequencySort("cccaaa"))
+print(problem.frequencySort("Aabb"))
