699df0eca8274e21291f0ce1173f48465e544c2c
taejin <taejin5314@gmail.com>
2021-10-22 13:49:05 -0400

    initial code for minimum index sum of two lists

---
+++ minimumIndexSumOfTwoLists.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+  def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
+    return 0
+
+problem = Solution()
+print(problem.findRestaurant(["Shogun","Tapioca Express","Burger King","KFC"], ["Piatti","The Grill at Torrey Pines","Hungry Hunter Steakhouse","Shogun"]))
+print(problem.findRestaurant(["Shogun","Tapioca Express","Burger King","KFC"], ["KFC","Shogun","Burger King"]))
+print(problem.findRestaurant(["Shogun","Tapioca Express","Burger King","KFC"], ["KFC","Burger King","Tapioca Express","Shogun"]))
+print(problem.findRestaurant(["Shogun","Tapioca Express","Burger King","KFC"], ["KNN","KFC","Burger King","Tapioca Express","Shogun"]))
+print(problem.findRestaurant(["KFC"], ["KFC"]))
