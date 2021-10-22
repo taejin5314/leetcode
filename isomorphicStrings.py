9b48605145802a69281a710266c27165bb866d9a
taejin <taejin5314@gmail.com>
2021-10-22 13:25:41 -0400

    initial code for isomorphic strings

---
+++ isomorphicStrings.py
@@ -0,0 +1,13 @@
+class Solution:
+  def isIsomorphic(self, s: str, t: str) -> bool:
+    hashMap = {}
+
+    for c in s:
+      print(c)
+
+    return 0
+
+problem = Solution()
+print(problem.isIsomorphic('egg', 'add'))
+print(problem.isIsomorphic('foo', 'bar'))
+print(problem.isIsomorphic('paper', 'title'))
--- sortCharactersByFrequency.py
+++ sortCharactersByFrequency.py
@@ -1,6 +1,21 @@
 class Solution:
   def frequencySort(self, s: str) -> str:
-    return 0
+    hashMap = {}
+
+    for c in s:
+      if c in hashMap:
+        hashMap[c] += 1
+      else:
+        hashMap[c] = 1
+    
+    hashMap = {k: v for k, v in sorted(hashMap.items(), key=lambda item: item[1], reverse=True)}
+
+    result = ''
+    for c in hashMap:
+      for _ in range(hashMap[c]):
+        result += c
+
+    return result
 
 problem = Solution()
 print(problem.frequencySort("tree"))
