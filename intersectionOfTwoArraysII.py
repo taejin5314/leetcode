658f5e721dea67476e20a31975d4eaa85da1ec33
taejin <taejin5314@gmail.com>
2021-10-24 14:11:39 -0400

    submit intersection of two array II

--- intersectionOfTwoArraysII.py
+++ intersectionOfTwoArraysII.py
@@ -2,7 +2,18 @@ from typing import List
 
 class Solution:
   def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-    print(nums1, nums2)
+    map = {}
+    res = []
+
+    for num in nums1:
+      map[num] = map.get(num, 0) + 1
+
+    for num in nums2:
+      if num in map and map[num] > 0:
+        res.append(num)
+        map[num] -= 1
+    
+    return res
 
 problem = Solution()
 print(problem.intersect([1,2,2,1], [2,2]))
