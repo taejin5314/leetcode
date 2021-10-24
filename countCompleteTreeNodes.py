2a2ed57c63f493a1b6a927bc0fcc4eed342aed6f
taejin <taejin5314@gmail.com>
2021-10-24 13:04:45 -0400

    initial code for leetcode daily challenge - count complete tree nodes

---
+++ countCompleteTreeNodes.py
@@ -0,0 +1,12 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+  def countNodes(self, root: Optional[TreeNode]) -> int:
+    return 0
+
+problem = Solution()
+print(problem.countNodes([1, 2, 3, 4, 5, 6]))
