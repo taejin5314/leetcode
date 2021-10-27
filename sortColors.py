5b3be5c96124a3230e5fbbef350c0418fa693aeb
taejin <taejin5314@gmail.com>
2021-10-26 20:40:43 -0400

    quick sort solution

--- sortColors.py
+++ sortColors.py
@@ -2,7 +2,21 @@ from typing import List
 
 class Solution:
   def sortColors(self, nums: List[int]) -> None:
-    return 0
+    left, right = 0, len(nums) - 1
+    i = 0
+
+    while i <= right:
+      if nums[i] == 0:
+        nums[left], nums[i] = nums[i], nums[left]
+        left += 1
+      elif nums[i] == 2:
+        nums[right], nums[i] = nums[i], nums[right]
+        i -= 1
+        right -= 1
+      
+      i += 1
+
+    return nums
 
 problem = Solution()
 print(problem.sortColors([2, 0, 2, 1, 1, 0]))
