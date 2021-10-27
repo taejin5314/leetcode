fa9e629afe46635e9ce6d71b726fe480fff25f2b
taejin <taejin5314@gmail.com>
2021-10-26 20:16:35 -0400

    initial code for sort colors

---
+++ sortColors.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+  def sortColors(self, nums: List[int]) -> None:
+    return 0
+
+problem = Solution()
+print(problem.sortColors([2, 0, 2, 1, 1, 0]))
+print(problem.sortColors([2, 0, 1]))
+print(problem.sortColors([0]))
+print(problem.sortColors([1]))
