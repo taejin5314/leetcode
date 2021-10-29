25b81f2510abc690fa4b59765ca11fa2ead20184
taejin <taejin5314@gmail.com>
2021-10-28 21:44:56 -0400

    initial code for rotting oranges

---
+++ rottingOranges.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+  def orangesRotting(self, grid: List[List[int]]) -> int:
+    return 0
+
+problem = Solution()
+print(problem.orangesRotting([[2,1,1],[1,1,0],[0,1,1]]))
+print(problem.orangesRotting([[2,1,1],[0,1,1],[1,0,1]]))
+print(problem.orangesRotting([[0,2]]))
