226975fd606860fdaf050a30861bb4fc05e8862d
taejin <taejin5314@gmail.com>
2021-10-30 18:08:48 -0400

    submit contains duplicate II

--- containsDuplicateII.py
+++ containsDuplicateII.py
@@ -2,8 +2,12 @@ from typing import List
 
 class Solution:
   def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-    print(nums)
-    return 0
+    map = {}
+    for i, num in enumerate(nums):
+      if num in map and i - map[num] <= k:
+        return True
+      map[num] = i
+    return False
 
 problem = Solution()
 print(problem.containsNearbyDuplicate([1, 2, 3, 1], 3))
