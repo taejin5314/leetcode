34bb020fcc2daaae0a0b12f40e93aa958b820716
taejin <taejin5314@gmail.com>
2021-11-01 22:59:21 -0400

    submit group anagrams

--- groupAnagrams.py
+++ groupAnagrams.py
@@ -1,8 +1,15 @@
-from typing import List
+from typing import  List
+from collections import defaultdict
 
 class Solution:
   def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-    return strs
+    map = defaultdict(list)
+
+    for str in strs:
+      sortedStr = ''.join(sorted(str))
+      map[sortedStr].append(str)
+
+    return map.values()
 
 problem = Solution()
 print(problem.groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
