712ae7ffbde756bd278c2b86974155667f81d23f
taejin <taejin5314@gmail.com>
2021-11-06 00:47:26 -0400

    submit daily leetcode

--- singleNumberIII.py
+++ singleNumberIII.py
@@ -1,8 +1,16 @@
 from typing import List
+from collections import defaultdict
 
 class Solution:
   def singleNumber(self, nums: List[int]) -> List[int]:
-    print(nums)
+    map = defaultdict(int)
+
+    for num in nums:
+      if map[num] == 1:
+        del map[num]
+      else:
+        map[num] += 1
+    return map.keys()
 
 problem = Solution()
 print(problem.singleNumber([1, 2, 1, 3, 2, 5]))
