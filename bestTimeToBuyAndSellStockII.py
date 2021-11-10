e4be5fa0f7e08feefc558e848b7226cf824c5ade
taejin <taejin5314@gmail.com>
2021-11-09 20:36:23 -0500

    submit daily;

--- bestTimeToBuyAndSellStockII.py
+++ bestTimeToBuyAndSellStockII.py
@@ -2,7 +2,12 @@ from typing import List
 
 class Solution:
   def maxProfit(self, prices: List[int]) -> int:
-    return prices
+    res = 0
+
+    for i in range(1, len(prices)):
+      if prices[i] > prices[i-1]:
+        res += (prices[i] - prices[i-1])
+    return res
 
 problem = Solution()
 print(problem.maxProfit([7, 1, 5, 3, 6, 4]))
