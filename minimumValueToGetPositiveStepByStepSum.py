63e93d1c7c9d6b8ffff561c5c0673dea31ab4c9b
taejin <taejin5314@gmail.com>
2021-11-10 21:17:33 -0500

    submit daily

--- minimumValueToGetPositiveStepByStepSum.py
+++ minimumValueToGetPositiveStepByStepSum.py
@@ -2,8 +2,13 @@ from typing import List
 
 class Solution:
   def minStartValue(self, nums: List[int]) -> int:
-    return nums
+    sum, temp = nums[0], nums[0]
+    for i in nums[1:]:
+      temp += i
+      sum = min(sum, temp)
     
+    return max(1-sum, 1)
+
 problem = Solution()
 print(problem.minStartValue([-3, 2, -3, 4, 2]))
 print(problem.minStartValue([1, 2]))
