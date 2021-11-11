082858b21a29fbb64aebd74ba3cc4046f9990194
taejin <taejin5314@gmail.com>
2021-11-10 21:03:05 -0500

    leetcode daily

---
+++ minimumValueToGetPositiveStepByStepSum.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+  def minStartValue(self, nums: List[int]) -> int:
+    return nums
+    
+problem = Solution()
+print(problem.minStartValue([-3, 2, -3, 4, 2]))
+print(problem.minStartValue([1, 2]))
+print(problem.minStartValue([1, -2, -3]))
