52323b0062ee11804ef117d01c44a45313492edb
taejin <taejin5314@gmail.com>
2021-11-15 20:37:40 -0500

    submit leetcode

--- largestDivisibleSubset.py
+++ largestDivisibleSubset.py
@@ -2,7 +2,16 @@ from typing import List
 
 class Solution:
   def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
-    return nums
+    n = len(nums)
+    nums.sort()
+    res = [[num] for num in nums]
+
+    for i in range(n):
+      for j in range(i):
+        if nums[i] % nums[j] == 0 and len(res[i]) < len(res[j]) + 1:
+          res[i] = res[j] + [nums[i]]
+
+    return max(res, key=len)
 
 problem = Solution()
 print(problem.largestDivisibleSubset([1, 2, 3]))
