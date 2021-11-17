e27059221e2ae10af485701d78a3788b6053cc43
taejin <taejin5314@gmail.com>
2021-11-16 20:21:58 -0500

    submit daily

--- uniquePaths.py
+++ uniquePaths.py
@@ -1,6 +1,16 @@
 class Solution:
   def uniquePaths(self, m: int, n: int) -> int:
-    return m
+    result = [[0 for _ in range(n)] for _ in range(m)]
+
+    for i in range(m):
+      result[i][0] = 1
+    for i in range(n):
+      result[0][i] = 1
+    for i in range(1, m):
+      for j in range(1, n):
+        result[i][j] = result[i-1][j] + result[i][j-1]
+
+    return result[m-1][n-1]
 
 problem = Solution()
 print(problem.uniquePaths(3, 7))
