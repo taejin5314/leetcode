1a81a9b6576b8fb8920ab71237ec9792999bd496
taejin <taejin5314@gmail.com>
2021-11-17 22:40:12 -0500

    submit daily

--- findAllNumbersDisappearedInArray.py
+++ findAllNumbersDisappearedInArray.py
@@ -2,7 +2,11 @@ from typing import List
 
 class Solution:
   def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-    return nums
+    res = []
+    for i in range(1, len(nums)+1):
+      if i not in nums:
+        res.append(i)
+    return res
 
 problem = Solution()
 print(problem.findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1]))
