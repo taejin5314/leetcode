e4048ca8a86bd96662cfa4e64c292af972821339
taejin <taejin5314@gmail.com>
2021-11-17 22:38:40 -0500

    leetcode daily

---
+++ findAllNumbersDisappearedInArray.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+  def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+    return nums
+
+problem = Solution()
+print(problem.findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1]))
+print(problem.findDisappearedNumbers([1, 1]))
