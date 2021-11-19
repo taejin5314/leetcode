29fbdf30b62eaa77328a96064585ce9faa85ee51
taejin <taejin5314@gmail.com>
2021-11-18 19:36:56 -0500

    leetcode daily

---
+++ hammeringDistance.py
@@ -0,0 +1,7 @@
+class Solution:
+  def hammingDistance(self, x: int, y: int) -> int:
+    return x
+
+problem = Solution()
+print(problem.hammingDistance(1, 4))
+print(problem.hammingDistance(3, 1))
