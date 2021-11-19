52998342fec970c966e34716a06826b870341725
taejin <taejin5314@gmail.com>
2021-11-18 19:39:33 -0500

    submit leetcode

--- hammeringDistance.py
+++ hammeringDistance.py
@@ -1,6 +1,14 @@
 class Solution:
   def hammingDistance(self, x: int, y: int) -> int:
-    return x
+    res = x ^ y
+    count = 0
+
+    while res > 0:
+      if res & 1 == 1:
+        count += 1
+      res = res >> 1
+    
+    return count
 
 problem = Solution()
 print(problem.hammingDistance(1, 4))
