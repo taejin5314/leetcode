70c7f0baa216f12bb27f21b5834e4847aae4ed35
taejin <taejin5314@gmail.com>
2021-11-22 20:26:13 -0500

    leetcode

--- deleteNodeInBST.py
+++ deleteNodeInBST.py
@@ -8,6 +8,25 @@ class TreeNode:
 
 class Solution:
   def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+    if not root:
+      return None
+
+    if root.val == key:
+      if root.left:
+        left_right_most = root.left
+        while left_right_most.right:
+          left_right_most = left_right_most.right
+        
+        left_right_most.right = root.right
+
+        return root.left
+      else:
+        return root.right
+    elif root.val > key:
+      root.left = self.deleteNode(root.left, key)
+    else:
+      root.right = self.deleteNode(root.right, key)
+      
     return root
 
 problem = Solution()
