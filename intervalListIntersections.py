4882d723a1e05351c1d7d4d4bf020f48da64903d
taejin <taejin5314@gmail.com>
2021-11-23 20:14:59 -0500

    submit daily

--- intervalListIntersections.py
+++ intervalListIntersections.py
@@ -2,7 +2,16 @@ from typing import List
 
 class Solution:
   def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
-    return firstList
+    combined = sorted(A + B, key=itemgetter(0))
+    intersection = []
+    currentEnd = -1
+
+    for start, end in combined:
+      if start <= currentEnd:
+        intersection.append([start, min(currentEnd, end)])
+      currentEnd = max(currentEnd, end)        
+
+    return intersection
 
 problem = Solution()
 print(problem.intervalIntersection([[0,2],[5,10],[13,23],[24,25]], [[1,5],[8,12],[15,24],[25,26]]))
