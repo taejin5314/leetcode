f8fe2f7fcb6d4bc2cc223b150bc207351b005047
taejin <taejin5314@gmail.com>
2021-11-23 20:14:02 -0500

    daily

---
+++ intervalListIntersections.py
@@ -0,0 +1,8 @@
+from typing import List
+
+class Solution:
+  def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
+    return firstList
+
+problem = Solution()
+print(problem.intervalIntersection([[0,2],[5,10],[13,23],[24,25]], [[1,5],[8,12],[15,24],[25,26]]))
