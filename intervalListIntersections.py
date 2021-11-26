50335952f243bda355aa5bfbae980d34212112a2
taejin <taejin5314@gmail.com>
2021-11-25 21:21:22 -0500

    leetcode

--- intervalListIntersections.py
+++ intervalListIntersections.py
@@ -12,5 +12,6 @@ class Solution:
       currentEnd = max(currentEnd, end)        
 
     return intersection
+    
 problem = Solution()
 print(problem.intervalIntersection([[0,2],[5,10],[13,23],[24,25]], [[1,5],[8,12],[15,24],[25,26]]))
