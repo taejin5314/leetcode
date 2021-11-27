4e5d0beeae97b2d538c37aae241c37ed9918b6e5
taejin <taejin5314@gmail.com>
2021-11-27 15:23:50 -0500

    daily leetcode

---
+++ productOfArrayExceptSelf.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+  def productExceptSelf(self, nums: List[int]) -> List[int]:
+    return nums
+
+problem = Solution()
+print(problem.productExceptSelf([1,2,3,4]))
+print(problem.productExceptSelf([-1,-1,0,-3,-3]))
