1924ce183b9dfa821a86b5861ae557894a09b739
taejin <taejin5314@gmail.com>
2021-11-27 15:27:30 -0500

    submit daily

--- productOfArrayExceptSelf.py
+++ productOfArrayExceptSelf.py
@@ -2,7 +2,18 @@ from typing import List
 
 class Solution:
   def productExceptSelf(self, nums: List[int]) -> List[int]:
-    return nums
+    answer = 1
+    n = len(nums)
+    res = []
+
+    for i in range(n):
+      res.append(answer)
+      answer *= nums[i]
+    answer = 1
+    for i in range(n-1, -1, -1):
+      res[i] = res[i] * answer
+      answer *= nums[i]
+    return res
 
 problem = Solution()
 print(problem.productExceptSelf([1,2,3,4]))
