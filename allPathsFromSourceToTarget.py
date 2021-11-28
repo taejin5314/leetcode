c2cef20a07be5e64bd7d40ec0d099442ece39809
taejin <taejin5314@gmail.com>
2021-11-28 13:03:57 -0500

    submit leetcode

--- allPathsFromSourceToTarget.py
+++ allPathsFromSourceToTarget.py
@@ -2,9 +2,24 @@ from typing import List
 
 class Solution:
   def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-    return graph
+    result = []
+    temp = []
+
+    def dfs(node):
+      if node == len(graph) - 1:
+        result.append(temp.copy())
+        return
+
+      for i in graph[node]:
+        temp.append(i)
+        dfs(i)
+        temp.pop()
+    
+    temp.append(0)
+    dfs(0)
+    return result
 
 problem = Solution()
 print(problem.allPathsSourceTarget([[1,2], [3], [3], []]))
 print(problem.allPathsSourceTarget([[4,3,1],[3,2,4],[3],[4],[]]))
-print(problem.allPathsSourceTarget([1], []))
+print(problem.allPathsSourceTarget([[1],[]]))
