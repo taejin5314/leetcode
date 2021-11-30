862e5b374c2897931b445fa011d705337d4adbd7
taejin <taejin5314@gmail.com>
2021-11-29 20:37:37 -0500

    leetcode

--- allPathsFromSourceToTarget.py
+++ allPathsFromSourceToTarget.py
@@ -18,7 +18,6 @@ class Solution:
     temp.append(0)
     dfs(0)
     return result
-
 problem = Solution()
 print(problem.allPathsSourceTarget([[1,2], [3], [3], []]))
 print(problem.allPathsSourceTarget([[4,3,1],[3,2,4],[3],[4],[]]))
