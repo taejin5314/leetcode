0e1c7c6658b8457c0948054a8cdeb2055c0c42b7
taejin <taejin5314@gmail.com>
2021-11-30 20:11:49 -0500

    leetcode daily

---
+++ houseRobber.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+  def rob(self, nums: List[int]) -> int:
+    return nums
+
+problem = Solution()
+print(problem.rob([1,2,3,1]))
+print(problem.rob([2,7,9,3,1]))
