e309bda28a1aee6eed68a85b1c416f5a70af9462
taejin <taejin5314@gmail.com>
2021-11-30 20:13:35 -0500

    submit leetcode

--- houseRobber.py
+++ houseRobber.py
@@ -2,7 +2,10 @@ from typing import List
 
 class Solution:
   def rob(self, nums: List[int]) -> int:
-    return nums
+    max_3_before, max_2_before, adjacent = 0, 0, 0
+    for cur in nums:
+      max_3_before, max_2_before, adjacent = max_2_before, adjacent, max(max_3_before + cur, max_2_before + cur)
+    return max(max_2_before, adjacent)
 
 problem = Solution()
 print(problem.rob([1,2,3,1]))
