5b9afd9519283eaa6fd8021ba6bcb2856c2a98b3
taejin <taejin5314@gmail.com>
2021-12-01 20:12:23 -0500

    leetcode

--- houseRobber.py
+++ houseRobber.py
@@ -7,6 +7,7 @@ class Solution:
       max_3_before, max_2_before, adjacent = max_2_before, adjacent, max(max_3_before + cur, max_2_before + cur)
     return max(max_2_before, adjacent)
 
+
 problem = Solution()
 print(problem.rob([1,2,3,1]))
 print(problem.rob([2,7,9,3,1]))
