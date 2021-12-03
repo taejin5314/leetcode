1854ea088ac494560aacc07b0f39e4edf05fc59b
taejin <taejin5314@gmail.com>
2021-12-02 23:16:03 -0500

    submit leetcode

--- maximumProductSubarray.py
+++ maximumProductSubarray.py
@@ -2,7 +2,19 @@ from typing import List
 
 class Solution:
   def maxProduct(self, nums: List[int]) -> int:
-    return nums
+    prev_max = nums[0]
+    prev_min = nums[0]
+    max_to_n = nums[0]
+    min_to_n = nums[0]
+    ans = nums[0]
+
+    for i in nums[1:]:
+      max_to_n = max(max(prev_max*i, prev_min*i), i)
+      min_to_n = min(min(prev_max*i, prev_min*i), i)
+      prev_max = max_to_n
+      prev_min = min_to_n
+      ans = max(ans, max_to_n)
+    return ans
 
 problem = Solution()
 print(problem.maxProduct([2, 3, -2, 4]))
