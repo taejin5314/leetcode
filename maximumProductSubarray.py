ad6332085a90c515683915669e501012fc6e8245
taejin <taejin5314@gmail.com>
2021-12-02 23:11:10 -0500

    leetcode daily;

---
+++ maximumProductSubarray.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+  def maxProduct(self, nums: List[int]) -> int:
+    return nums
+
+problem = Solution()
+print(problem.maxProduct([2, 3, -2, 4]))
+print(problem.maxProduct([-2, 0, -1]))
