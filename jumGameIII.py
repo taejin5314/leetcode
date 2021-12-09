3300c2fd901cc51e4ad864be1b682d424231582f
taejin <taejin5314@gmail.com>
2021-12-08 19:06:58 -0500

    leetcode daily

---
+++ jumGameIII.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+  def canReach(self, arr: List[int], start: int) -> bool:
+    return arr
+
+problem = Solution()
+print(problem.canReach([4, 2, 3, 0, 3, 1, 2], 5))
+print(problem.canReach([4, 2, 3, 0, 3, 1, 2], 0))
+print(problem.canReach([3, 0, 2, 1, 2], 2))
