d59f1e96aca9074f1b802f1e4a5131cb7d1ae12a
taejin <taejin5314@gmail.com>
2021-12-08 19:09:02 -0500

    submit leetcode

--- jumGameIII.py
+++ jumGameIII.py
@@ -2,7 +2,27 @@ from typing import List
 
 class Solution:
   def canReach(self, arr: List[int], start: int) -> bool:
-    return arr
+    queue = [start]
+    visited = [False] * len(arr)
+
+    while queue:
+      curr = queue[0]
+      queue.pop(0)
+
+      if arr[curr] == 0:
+        return True
+
+      visited[curr] = True
+      right = curr + arr[curr]
+      left = curr - arr[curr]
+
+      if right < len(arr) and not visited[right]:
+        queue.append(right)
+
+      if left >= 0 and not visited[left]:
+        queue.append(left)
+
+    return False
 
 problem = Solution()
 print(problem.canReach([4, 2, 3, 0, 3, 1, 2], 5))
