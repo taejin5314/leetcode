c196375792f4cb7dcf87343e1435ca9b2d5b4f40
taejin <taejin5314@gmail.com>
2021-12-13 20:22:09 -0500

    submit leetcode

--- rangeSumOfBST.py
+++ rangeSumOfBST.py
@@ -5,5 +5,11 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
-        
+  def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+    if root is None:
+      return 0
+    if root.val > high:
+      return self.rangeSumBST(root.left, low, high)
+    if root.val < low:
+      return self.rangeSumBST(root.right, low, high)
+    return root.val + self.rangeSumBST(root.left, low, high) + self.rangeSumBST(root.right, low, high)
