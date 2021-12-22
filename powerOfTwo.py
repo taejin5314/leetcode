847629094240ec5552c52615e8a31dad19bcefe2
taejin <taejin5314@gmail.com>
2021-12-21 23:24:49 -0500

    leetcode

--- powerOfTwo.py
+++ powerOfTwo.py
@@ -2,6 +2,7 @@ class Solution:
   def isPowerOfTwo(self, n: int) -> bool:
     return n > 0 and n & n - 1 == 0
 
+
 problem = Solution()
 print(problem.isPowerOfTwo(1))
 print(problem.isPowerOfTwo(16))
