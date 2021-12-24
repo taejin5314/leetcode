edd6def176c2b8d62ab557e61f8bc5762b622bba
taejin <taejin5314@gmail.com>
2021-12-23 21:22:56 -0500

    leetcode daily

---
+++ mergeIntervals.py
@@ -0,0 +1,8 @@
+from typing import List
+
+class Solution:
+  def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+    return intervals
+
+problem = Solution()
+print(problem.merge([[1,3], [2,6], [8,10], [15,18]]))
