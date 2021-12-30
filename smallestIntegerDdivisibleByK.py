50b9e2976eb5e350fb417dd5f6656e5bb32892a0
taejin <taejin5314@gmail.com>
2021-12-30 18:34:13 -0500

    submit leetcode

--- smallestIntegerDdivisibleByK.py
+++ smallestIntegerDdivisibleByK.py
@@ -1,6 +1,13 @@
 class Solution:
   def smallestRepunitDivByK(self, k: int) -> int:
-    return k
+    if not k % 2 or not k % 5:
+      return -1
+    n = length = 1
+    while True:
+      if not n % k:
+        return length
+      length += 1
+      n = 10 * n + 1
 
 problem = Solution()
 print(problem.smallestRepunitDivByK(1))
