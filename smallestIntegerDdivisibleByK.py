4df3606458617c731feab747cf920259e7e0feb0
taejin <taejin5314@gmail.com>
2021-12-30 18:32:24 -0500

    leetcode daily

---
+++ smallestIntegerDdivisibleByK.py
@@ -0,0 +1,8 @@
+class Solution:
+  def smallestRepunitDivByK(self, k: int) -> int:
+    return k
+
+problem = Solution()
+print(problem.smallestRepunitDivByK(1))
+print(problem.smallestRepunitDivByK(2))
+print(problem.smallestRepunitDivByK(3))
