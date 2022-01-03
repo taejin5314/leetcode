409434f6ce0185cf3bb3cf5c43e777383057bd9d
taejin <taejin5314@gmail.com>
2022-01-02 19:32:39 -0500

    leetcode daily

---
+++ findTheTownJudge.py
@@ -0,0 +1,8 @@
+class Solution:
+  def findJudge(self, n: int, trust: List[List[int]]) -> int:
+    return n
+
+problem = Solution()
+print(problem.findJudge([[1,2]]))
+print(problem.findJudge([[1,3], [2,3]]))
+print(problem.findJudge([[1,3], [2,3], [3,1]]))
