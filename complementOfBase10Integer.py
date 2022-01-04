ac0a8c2bd66e42a9d254ca177d2a9920f53598b0
taejin <taejin5314@gmail.com>
2022-01-03 22:06:28 -0500

    leetcode submit

--- complementOfBase10Integer.py
+++ complementOfBase10Integer.py
@@ -1,6 +1,12 @@
 class Solution:
   def bitwiseComplement(self, n: int) -> int:
-    return n
+    x=''
+    for i in bin(n)[2:]:
+      if i == '1':
+        x += '0'
+      else:
+        x += '1'
+    return int(x,2)
 
 problem = Solution()
 print(problem.bitwiseComplement(5))
