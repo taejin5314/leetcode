ce2190cf2711dab1e8627290d9535b39444f962b
taejin <taejin5314@gmail.com>
2022-01-03 22:03:41 -0500

    leetcode daily

---
+++ complementOfBase10Integer.py
@@ -0,0 +1,8 @@
+class Solution:
+  def bitwiseComplement(self, n: int) -> int:
+    return n
+
+problem = Solution()
+print(problem.bitwiseComplement(5))
+print(problem.bitwiseComplement(7))
+print(problem.bitwiseComplement(10))
