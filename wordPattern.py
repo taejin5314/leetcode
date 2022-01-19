15d364ca27b299a1dfee3468ccc79ac7546e3e1e
taejin <taejin5314@gmail.com>
2022-01-18 20:26:54 -0500

    leetcode

--- wordPattern.py
+++ wordPattern.py
@@ -1,4 +1,5 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
         str = s.split(' ')
+        
         return len(set(pattern)) == len(set(str)) == len(set(zip(pattern, str))) and len(pattern) == len(str)
