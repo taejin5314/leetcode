88a33949dd661d7342becd1fac3fb7812c361431
taejin <taejin5314@gmail.com>
2022-02-01 20:26:15 -0500

    leetcode daily

---
+++ findAllAnagramsInString.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        start = 0 
+        dic_pattern = collections.Counter(pattern)
+        dic_s = {}
+        result = []
+        
+        for end in range(len(s)):
+            if s[end] not in dic_s:
+                dic_s[s[end]] = 1
+            else:
+                dic_s[s[end]] += 1
+            
+            if dic_s == dic_pattern:
+                result.append(start)
+                
+            if (end - start +1) >= len(pattern): 
+                if dic_s[s[start]] > 1:
+                    dic_s[s[start]] -= 1
+                else:
+                    del dic_s[s[start]]
+                start += 1
+        return result  
