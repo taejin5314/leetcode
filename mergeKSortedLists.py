f29e5b660492f3f3e45ce9cb8c9d396ac7587351
taejin <taejin5314@gmail.com>
2022-02-04 20:47:53 -0500

    leetcode daily

---
+++ mergeKSortedLists.py
@@ -0,0 +1,18 @@
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        values, head, pointer = [], None, None
+        
+        for l in lists:
+            while l:
+                heappush(values, l.val)
+                l = l.next
+        
+        while values:
+            if head == None:
+                head = ListNode(heappop(values))
+                pointer = head
+            else:
+                pointer.next = ListNode(heappop(values))
+                pointer = pointer.next
+        
+        return head
