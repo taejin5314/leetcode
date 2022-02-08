b8bc65bbc1283ae1e67a270a6557830fce9078ce
taejin <taejin5314@gmail.com>
2022-02-07 21:34:59 -0500

    leetcode daily

---
+++ addDigits.py
@@ -0,0 +1,6 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        i = sum([int(x) for x in str(num)])
+        while i > 9:
+            i = sum([int(x) for x in str(i)])
+        return i
