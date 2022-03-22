f71b6458928830ee88b0893a4a84aad97ad9bf8d
taejin <taejin5314@gmail.com>
2022-03-21 20:12:00 -0400

    leetcode daily

--- smallestStringWithAGivenNumericValue.py
+++ smallestStringWithAGivenNumericValue.py
@@ -1,3 +1,21 @@
 class Solution:
-    def getSmallestString(self, n: int, k: int) -> str:
-        
+  def getSmallestString(self, n: int, k: int) -> str:
+    res = []
+    while n > 0:
+      diff = n * 26 - k
+			
+      if diff == 0:
+        res.extend('z' * n)
+        break
+      elif diff > 25:
+        let = 'a'
+        k -= 1
+      else:
+        code = ord('a') + 26 - diff - 1
+        let = chr(code)
+        k -= 26 - diff
+         
+      res.append(let)
+      n -= 1
+         
+    return ''.join(res)
