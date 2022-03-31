e6b5469000ebda000eac8469ae52f0cb8bebe904
taejin <taejin5314@gmail.com>
2022-03-31 19:46:39 -0400

    leetcode daily

--- splitArrayLargestSum.py
+++ splitArrayLargestSum.py
@@ -1,35 +1,15 @@
 class Solution:
     def splitArray(self, nums: List[int], m: int) -> int:
-        prefixSum = []
-        curSum = 0 
-        for num in nums: 
-            curSum += num
-            prefixSum.append(curSum)
-        self.prefixSum = prefixSum   
-        memo = dict()
-        minMax = self.helper(0, m-1, memo)
-        return minMax
+        def isPossible(maxSum):
+            curr = count = 0
+            for i in nums:
+                count += (i + curr > maxSum)
+                curr = curr + i if i + curr <= maxSum else i
+            return count + 1 <= m
         
-    def getSum(self, start: int, end: int) -> int: 
-        res = self.prefixSum[end]
-        res -= self.prefixSum[start-1] if start-1 >= 0 else 0 
-        return res
-    
-    def helper(self, index: int, splits: int, memo: dict) -> int: 
-        if splits == 0: 
-            subarray = self.getSum(index, len(self.prefixSum)-1)
-            return subarray
-        
-        key = (index, splits) 
-        if key in memo: 
-            return memo[key]
-        
-        minMax = float('inf')
-        maxIndex = len(self.prefixSum)-splits
-        for i in range (index, maxIndex): 
-            subarray = self.getSum(index, i)
-            maxLeftover = self.helper(i+1, splits-1, memo)
-            maximum = max(subarray, maxLeftover) 
-            minMax = min(minMax, maximum)
-        memo[key] = minMax
-        return minMax  
+        lo, hi = max(nums), sum(nums)
+        while lo <= hi:
+            mid = (lo + hi) // 2
+            if isPossible(mid): hi = mid - 1
+            else: lo = mid + 1
+        return lo
