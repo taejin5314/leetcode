755a194bc31470fd59d56741466c3aa106ea62bf
taejin <taejin5314@gmail.com>
2022-04-01 19:49:06 -0400

    leetcode daily

--- reverseString.py
+++ reverseString.py
@@ -1,11 +1,6 @@
-from typing import List
-
 class Solution:
-  def reverseString(self, s: List[str]) -> None:
-    if len(s) < 2:
-      return s
-    s = self.reverseString(s[len(s)//2:]) + self.reverseString(s[:len(s)//2])
-    return s
-
-problem = Solution()
-print(problem.reverseString(["h","e","l","l","o"]))
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        s[:] = s[::-1]
