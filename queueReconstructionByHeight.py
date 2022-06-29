2bb60c51a0c57030db31c7b4d559322479e3751a
taejin <taejin5314@gmail.com>
2022-06-28 20:43:21 -0400

    leetcode daily

---
+++ queueReconstructionByHeight.py
@@ -0,0 +1,7 @@
+class Solution:
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        people.sort(key = lambda x: [-x[0], x[1]])
+        ans = []
+        for h, k in people:
+            ans = ans[:k] + [[h, k]] + ans[k:]
+        return ans
