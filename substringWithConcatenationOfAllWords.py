db7a4b6ab006adfe421f83bcdd3f4ce748fb7ef1
taejin <taejin5314@gmail.com>
2022-08-12 20:05:18 -0400

    leetcode daily

---
+++ substringWithConcatenationOfAllWords.py
@@ -0,0 +1,5 @@
+class Solution:
+    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+        if not words: return []
+        LS, M, N, C = len(s), len(words), len(words[0]), collections.Counter(words)
+        return [i for i in range(LS-M*N+1) if collections.Counter([s[a:a+N] for a in range(i,i+M*N,N)]) == C]
