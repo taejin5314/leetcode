c70f0b456b6f4ad941e0d7c32f713e683b88ea10
taejin <taejin5314@gmail.com>
2022-08-15 20:09:19 -0400

    leetcode daily

---
+++ firstUniqueCharacterInAString.py
@@ -0,0 +1,15 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        map = {}
+
+        for i, c in enumerate(s):
+            if c not in map:
+                map[c] = [1, i]
+            else:
+                map[c][0] += 1
+    
+        for c in map:
+            if map[c][0] == 1:
+                return map[c][1]
+
+        return -1
